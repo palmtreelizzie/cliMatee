45872a1e04114bebbc1ee5f38c09fd59a8998077
Glitch (steep-long-platypus) <none>
2020-02-29 21:27:57 +0000

    🚇🐂 Checkpoint ./style.css:12065436/418:12065445/38 ./community.html:12065445/265 ./cutdown.html:12065462/10897 ./index.html:677248/434:12065436/49:12065445/3987 ./script.js:12065445/1235 ./recycling.html:12065462/3590

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-$(function() {
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 1) {
-      $("header nav").addClass("changeColor");
-    }
-    if ($(this).scrollTop() < 1) {
-      $("header nav").removeClass("changeColor");
-    }
-  });
-});
+<script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+function () {
+  (window).scroll(function () {
+      if ((this).scrollTop() > 1) {
+            ('nav').addClass('changeColor')
+        }
+        if ((this).scrollTop() < 1) {
+            ('nav').removeClass('changeColor')
+        }
+    })
+}
